b05453c52953670a286618fa021f6e657d756593
gnehcein <65959195+gnehcein@users.noreply.github.com>
2021-05-12 19:42:41 +0800

    Add files via upload

--- go-beginner/接口接受指针和所指对象.go
+++ go-beginner/接口接受指针和所指对象.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-type in interface {
+type in21 interface {
 	x(int)
 	y(int)
 }
-type in2 interface {
+type in22 interface {
 	z(int)
 }
 
@@ -23,7 +23,7 @@ func (t *T1) z(int)  {	//指针方法
 
 }
 func main() {
- 	var i1 in=T1{}	//接口对着sturct，不能接受struct的指针的方法，接口的视角把结构体和其指针实现的方法隔离了。
-	var i2 in2 =&T1{}
+ 	var i1 in21=&T1{}	//指针带着struct和指针本身的方法
+	var i2 in22 =T1{}	//报错，因为结构体只带着自身的方法
 	fmt.Println(i1,i2)
 }
