cb051348c3f8b7da1936bb4582ef5b194af1fb97
gnehcein <niechenggong@163.com>
2021-07-30 23:10:11 +0800

    7.30

--- main/context.go
+++ main/context.go
@@ -6,43 +6,43 @@ import (
 	"time"
 )
 
-func main()  {
-	ctx:=context.Background()
-	ctx1,cancel1:=context.WithCancel(ctx)
-	ctx2,_:=context.WithCancel(ctx1)
+func main() {
+	ctx := context.Background()
+	ctx1, cancel1 := context.WithCancel(ctx)
+	ctx2, _ := context.WithCancel(ctx1)
 	go f1(ctx1)
 	go f2(ctx2)
-	time.Sleep(5*time.Second)
+	time.Sleep(5 * time.Second)
 	cancel1()
 
-	time.Sleep(10*time.Second)
+	time.Sleep(10 * time.Second)
 
 }
-func f1(ctx1 context.Context){
-	for  {
+func f1(ctx1 context.Context) {
+	for {
 		select {
 		case <-ctx1.Done():
 			defer fmt.Println("f1结束了")
 			return
 		default:
-			time.Sleep(3*time.Second)
+			time.Sleep(3 * time.Second)
 			fmt.Println("f1工作")
 
 		}
 	}
 }
-func f2(ctx2 context.Context){
-	ctx:=context.Background()
+func f2(ctx2 context.Context) {
+	ctx := context.Background()
 	go f2a(ctx)
-	time.Sleep(2*time.Second)
+	time.Sleep(2 * time.Second)
 }
-func f2a(ctx1 context.Context){
-	for  {
+func f2a(ctx1 context.Context) {
+	for {
 		select {
 		case <-ctx1.Done():
 		default:
-			time.Sleep(1*time.Second)
-			fmt.Println("f2a工作了",time.Now())
+			time.Sleep(1 * time.Second)
+			fmt.Println("f2a工作了", time.Now())
 
 		}
 	}
